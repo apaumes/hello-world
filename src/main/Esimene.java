445d5d185a536f5a73443e961483981e1619fb78
Anni Paumees <anni.paumees@gmail.com>
2016-11-08 21:23:39 +0200

    MinuEsimeneProgramm
    MinuEsimeneProgramm üleslaadimine GitHub.com-i

---
+++ src/main/Esimene.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by Anni on 6.11.2016.
+ */
+public class Esimene {
+    public static void main(String[] args) {
+        System.out.println("Tere minu esimene programm!");
+    }
+}
